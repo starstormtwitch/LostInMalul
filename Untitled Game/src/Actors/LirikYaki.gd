914405463a027ec1a24dc8227f055d3698d0f3aa
SLgeneration <slgeneration93@gmail.com>
2021-09-06 12:01:06 -0600

    added trail to player, fixed hitboxes and hurtboxes
    still some touch ups needed with direction facing when only moving vertical
    Also hurt animation issues. Also need into image quality issue.

--- Untitled Game/src/Actors/LirikYaki.gd
+++ Untitled Game/src/Actors/LirikYaki.gd
@@ -1,11 +1,18 @@
 extends Actor
 
+const trail_scene = preload("res://src/Helpers/Trail.tscn")
+
 var _isAttacking = false
 var _canTakeDamage = false
 var _directionFacing = Vector2.ZERO
+var _trail = []
 var _invincibilityTimer = Timer.new()
 
 onready var sprite = $Sprite
+onready var leftFacingCollider = $LeftFacingCollider
+onready var rightFacingCollider = $RightFacingCollider
+onready var leftFacingHurtbox = $HurtArea/hurtboxFacingLeft
+onready var rightFacingHurtBox = $HurtArea/hurtboxFacingRight
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,10 +20,12 @@ func _ready():
 	_invincibilityTimer.one_shot = true
 	add_child(_invincibilityTimer)
 	_invincibilityTimer.start(3)
+	
 	_health = 10
 	_acceleration = .1
 	_speed = 600
 	_directionFacing.x = .1;
+	$TrailTimer.connect("timeout", self, "add_trail")
 	$AnimationTree.active = true
 
 
@@ -38,15 +47,19 @@ func _on_invincibility_timeout():
 	_canTakeDamage = true
 
 
-func _on_attack_area_entered(area):
-	if area.is_in_group("hurtbox") && area.get_parent() != null && area.get_parent().has_method("take_damage"):
-		area.get_parent().take_damage(1, _directionFacing, 100000)
-		$AnimationTree.get("parameters/playback").travel("Hurt")
+func add_trail():
+	if(get_parent() != null):
+		var trail      = trail_scene.instance()
+		trail.player   = self
+		trail.position = self.position
+		get_parent().add_child(trail)
+		_trail.push_front(trail)
 
 
 func take_damage(damage: int, direction: Vector2, force: float) -> void:
 	if _canTakeDamage:
 		_canTakeDamage = false
+		$AnimationTree.get("parameters/playback").travel("Hurt")
 		_invincibilityTimer.start(2)
 		.take_damage(damage, direction, force)
 
@@ -60,29 +73,47 @@ func evaluatePlayerInput() -> Vector2:
 	if(direction != Vector2.ZERO):
 		_directionFacing = direction
 		
-	_flipSpriteIfNeeded(direction.x)
+	_handleCollidersForDifferentDirections(direction.x)
 	
 	#check attack inputs
 	if Input.is_action_just_pressed("side_swipe_attack") or Input.is_action_pressed("side_swipe_attack"):
 		$AnimationTree.get("parameters/playback").travel("SideSwipe")
+		$AnimationTree.set("parameters/SideSwipe/blend_position", _directionFacing.x)
 		_isAttacking = true
 		return Vector2.ZERO
-		
+	
+	#set blend postion for hurt animation, so next time player is hit hes facing correct direction
+	$AnimationTree.set("parameters/Hurt/blend_position", _directionFacing.x)
+	
 	#set animation for direction and return for movement
 	if direction == Vector2.ZERO:
 		$AnimationTree.get("parameters/playback").travel("Idle")
+		$AnimationTree.set("parameters/Idle/blend_position", _directionFacing.x)
 	else:
 		$AnimationTree.get("parameters/playback").travel("Walk")
+		$AnimationTree.set("parameters/Walk/blend_position", direction.x)
+	
 	return direction
 
 
-#Flip sprite according to what input pressed, if 0 nothing is pressed and we leave as is
-func _flipSpriteIfNeeded(x_direction):
+#Disable and enable colliders depending which direction your facing
+func _handleCollidersForDifferentDirections(x_direction):
 	if x_direction > 0:
-		sprite.scale.x = 1
+		leftFacingCollider.disabled = true
+		rightFacingCollider.disabled = false
+		leftFacingHurtbox.disabled = true
+		rightFacingHurtBox.disabled = false
 	elif x_direction < 0:
-		sprite.scale.x = -1
+		leftFacingCollider.disabled = false
+		rightFacingCollider.disabled = true
+		leftFacingHurtbox.disabled = false
+		rightFacingHurtBox.disabled = true
 
 
 func _finishedAttack():
 	_isAttacking = false
+
+
+func _on_attack_area_entered(area):
+	if area.is_in_group("hurtbox") && area.get_parent() != null && area.get_parent().has_method("take_damage"):
+		area.get_parent().take_damage(1, _directionFacing, 100000)
