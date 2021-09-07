245feabdcb68bcb30ddb2b8500acf18663c04d7d
SLgeneration <slgeneration93@gmail.com>
2021-09-07 16:43:49 -0600

    add timer to return camera control

--- Untitled Game/src/Levels/LevelWithRoomsMockup.gd
+++ Untitled Game/src/Levels/LevelWithRoomsMockup.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+var _LABEL_POSITION_OFFSET = 100
+
+var _lockCamera = false
+var _startCountDown = 5
+var _timerOn = false
 
 onready var camera = $Camera2D
 onready var player = $LirikYaki
 onready var firstLockCollision = $FirstRoomLockArea/FirstRoomLockCollision
 onready var cameraManager = CameraManager.new(player, camera)
-
-var _lockCamera = false
+onready var label = $CanvasLayer/Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +20,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	cameraManager.followPlayerIfUnlocked()
+	_handleTimerUpdate(delta)
 
 
 func _on_FirstRoomLockArea_area_entered(area):
@@ -24,3 +29,30 @@ func _on_FirstRoomLockArea_area_entered(area):
 	# an actual space on the map/scene
 	cameraManager.lockCameraToPosition(player.position)
 	firstLockCollision.disabled = true
+	_startTimer()
+
+
+func _handleTimerUpdate(delta):
+	_startCountDown -= delta
+	#print("Updating time: " + String(_startCountDown))
+	if _startCountDown > 0:
+		_updateTimerText(_startCountDown)
+	else:
+		_timerOn = false
+		hideTimerText()
+		cameraManager.unlockTheCamera()
+
+
+func _updateTimerText(time):
+	var roundedTime = ceil(time)
+	label.text = String(roundedTime)
+
+
+func hideTimerText():
+	label.text = ""
+
+
+func _startTimer():
+	_startCountDown = 5
+	_timerOn = true
+	label.text = "5"
