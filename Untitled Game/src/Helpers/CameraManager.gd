eb01d2854dcf6ac61df1d42d81d78337820cdfbe
SLgeneration <slgeneration93@gmail.com>
2021-09-08 12:59:31 -0600

    added camera smoothing
    camera smoothing fixed the issue with the camera teleporting,
    when changing camera position by a lot

--- Untitled Game/src/Helpers/CameraManager.gd
+++ Untitled Game/src/Helpers/CameraManager.gd
@@ -6,17 +6,23 @@ class_name CameraManager
 var _player: KinematicBody2D
 var _camera: Camera2D
 var _lockCamera = false
+var _currentCameraPosition: Vector2
+
+const _SMOOTHING = 0.05
 
 func _init(player, camera):
 	_camera = camera
 	_player = player
-
+	_currentCameraPosition = player.position
 
 # Updates camera to player's position when unlocked
 # Would call this in parent nodes _process function, to get camera updates when available
 func followPlayerIfUnlocked(): 
 	if !_lockCamera:
-		_camera.position = _player.position
+		var targetX = int(lerp(_currentCameraPosition.x, _player.position.x, _SMOOTHING))
+		var targetY = int(lerp(_currentCameraPosition.y, _player.position.y, _SMOOTHING))
+		_currentCameraPosition = Vector2(targetX, targetY)
+		_camera.position = _currentCameraPosition
 
 
 # Moves camera to positions, and disables camera updates until unlocked again
