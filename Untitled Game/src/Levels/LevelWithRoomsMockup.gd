b6296c9c828fb5d521384de441332e922a774b61
SLgeneration <slgeneration93@gmail.com>
2021-09-11 15:15:05 -0600

    add static body boundaries to first room
    keeps player from leaving when the camera is locked.
    In the future will look into a another solution that does not
    use static bodies.

--- Untitled Game/src/Levels/LevelWithRoomsMockup.gd
+++ Untitled Game/src/Levels/LevelWithRoomsMockup.gd
@@ -11,10 +11,12 @@ onready var player = $LirikYaki
 onready var firstLockArea = $FirstRoomLockArea
 onready var cameraManager = CameraManager.new(player, camera)
 onready var label = $CanvasLayer/Label
+onready var leftFirstRoomCollider = $FirstRoomBattleBoundary/LeftSide
+onready var rightFirstRoomCollider = $FirstRoomBattleBoundary/RightSide
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,27 +25,32 @@ func _process(delta):
 	_handleTimerUpdate(delta)
 
 
+# Event when hitting the First Room lock area, will lock camera, and player to that area
 func _on_FirstRoomLockArea_area_entered(area):
 	print("first collision detect: " + area.name)
 	# I use players position to lock the camera to, but it might be better to define
 	# an actual space on the map/scene
 	cameraManager.lockCameraToPosition(player.position)
-	firstLockArea.queue_free()
+	firstLockArea.queue_free() # disable any recurring events
+	enableFirstRoomBoundaries()
 	_startTimer()
 
 
+# Update countdown value left. Update timer text if not over, otherwise
+# hide the timer text, and disable boundaries
 func _handleTimerUpdate(delta):
 	if _timerOn:
 		_startCountDown -= delta
-		#print("Updating time: " + String(_startCountDown))
 		if _startCountDown > 0:
 			_updateTimerText(_startCountDown)
 		else:
 			_timerOn = false
 			hideTimerText()
 			cameraManager.unlockTheCamera()
+			hideFirstRoomBoundaries()
 
 
+# Round countdown timer up, and then display text
 func _updateTimerText(time):
 	var roundedTime = ceil(time)
 	label.text = String(roundedTime)
@@ -53,6 +60,20 @@ func hideTimerText():
 	label.text = ""
 
 
+func enableFirstRoomBoundaries():
+	# set deffered I believe waits until the next physics process happens,
+	# and then does the following action. This will make sure that the following
+	# physics changes actully happens, instead of just dissapearing when done normally
+	leftFirstRoomCollider.set_deferred("disabled", false)
+	rightFirstRoomCollider.set_deferred("disabled", false)
+
+
+func hideFirstRoomBoundaries():
+	leftFirstRoomCollider.set_deferred("disabled", true)
+	rightFirstRoomCollider.set_deferred("disabled", true)
+
+
+#Start the timer, and setup countdown text
 func _startTimer():
 	_startCountDown = 5
 	_timerOn = true
