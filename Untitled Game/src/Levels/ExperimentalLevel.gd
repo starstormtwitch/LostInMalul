8f102de745c26463a650d028522e55b809029a76
Ed <emartine8742@gmail.com>
2021-09-13 23:54:14 +0200

    Update ExperimentalLevel.gd
    fixed a wrong bathroom2 reference

--- Untitled Game/src/Levels/ExperimentalLevel.gd
+++ Untitled Game/src/Levels/ExperimentalLevel.gd
@@ -3,10 +3,12 @@ extends Node2D
 
 onready var player = get_node("YSort/Player")
 onready var camera = get_node("YSort/Player/Camera2D")
+onready var bathroom1 = get_node("YSort/BathroomScene1")
+onready var bathroom2 = get_node("YSort/BathroomScene2")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("YSort/BathroomScene1")._set_Camera_To_Area(camera)
+	bathroom1._set_Camera_To_Area(camera)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -14,8 +16,8 @@ func _ready():
 
 func _on_RightExit_body_entered(body):
 	if body == player:
-		get_node("YSort/BathroomScene2")._set_Camera_To_Area(camera)
+		bathroom2._set_Camera_To_Area(camera)
 
 func _on_LeftExit_body_entered(body):
 	if body == player:
-		get_node("YSort/BathroomScene2")._set_Camera_To_Area(camera)
+		bathroom1._set_Camera_To_Area(camera)
