8ea9ecf28aaf0655243a9ac050f3465bc5ba28bd
Ed <emartine8742@gmail.com>
2021-09-21 19:58:24 +0200

    some progress

--- Untitled Game/src/Helpers/CameraManager.gd
+++ Untitled Game/src/Helpers/CameraManager.gd
@@ -7,6 +7,7 @@ var _player: KinematicBody2D
 var _camera: Camera2D
 var _lockCamera = false
 var _currentCameraPosition: Vector2
+var _verbose = true
 
 const _SMOOTHING = 0.05
 
@@ -16,6 +17,27 @@ func _init(player, camera):
 	_camera = camera
 	_player = player
 	_currentCameraPosition = player.position
+	_camera.current = true
+	#_camera.smoothing_enabled = true
+	#_camera.smoothing_speed = _SMOOTHING
+
+func limitCameraToPositions(topLeft, bottomRight):
+	limitCameraToCoordinates(topLeft.position.y, topLeft.position.x, bottomRight.position.y, bottomRight.position.x)
+
+func limitCameraToCoordinates(top, left, bottom, right):
+	_camera.limit_top = top
+	_camera.limit_left = left
+	_camera.limit_bottom = bottom
+	_camera.limit_right = right
+	if _verbose:
+		print("new camera limits (top/left/bottom/right)")
+		print(_camera.limit_top)
+		print(_camera.limit_left)
+		print(_camera.limit_bottom)
+		print(_camera.limit_right)
+	
+func resetLimits():
+	limitCameraToCoordinates(-10000000, -10000000, 10000000, 10000000)
 
 # Updates camera to player's position when unlocked
 # Would call this in parent nodes _process function, to get camera updates when available
@@ -26,12 +48,10 @@ func followPlayerIfUnlocked():
 		_currentCameraPosition = Vector2(targetX, targetY)
 		_camera.position = _currentCameraPosition
 
-
 # Moves camera to positions, and disables camera updates until unlocked again
 func lockCameraToPosition(position):
 	_lockCamera = true
-	_camera.position
-
+	_camera.position = position
 
 #Unlock the camera for updates again
 func unlockTheCamera():
---
+++ Untitled Game/src/Levels/House.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+onready var _camera = $Camera2D
+onready var _player = get_node("YSort/Actors/LirikYaki")
+
+onready var cameraManager: CameraManager
+
+onready var _camPos_Bathroom_TopLeft = get_node("LevelBackground/CameraPositions/Bathroom_Top_Left")
+onready var _camPos_Bathroom_BottomRight = get_node("LevelBackground/CameraPositions/Bathroom_Bottom_Right")
+onready var _camPos_Bedroom_TopLeft = get_node("LevelBackground/CameraPositions/Bedroom_Top_Left")
+onready var _camPos_Bedroom_BottomRight = get_node("LevelBackground/CameraPositions/Bedroom_Bottom_Right")
+onready var _camPos_Studio_TopLeft = get_node("LevelBackground/CameraPositions/Studio_Top_Left")
+onready var _camPos_Studio_BottomRight = get_node("LevelBackground/CameraPositions/Studio_Bottom_Right")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	assert(_player, "Player Node does not exist.")
+	assert(_camera, "Camera2D Node does not exist")
+	cameraManager = CameraManager.new(_player, _camera)	
+	setCameraToBathroom()
+
+
+func _on_Transition_Bedroom_To_Bathroom_body_entered(body):
+	if body == _player:
+		setCameraToBathroom()
+
+func _on_Transition_Bathroom_To_Bedroom_body_entered(body):
+	if body == _player:
+		setCameraToBedroom()
+
+func _on_Bedroom_To_Studio_body_entered(body):
+	if body == _player:
+		setCameraToStudio()
+
+func _on_Studio_To_Bedroom_body_entered(body):
+	if body == _player:
+		setCameraToBedroom()
+
+func setCameraToBathroom():
+	cameraManager.limitCameraToPositions(_camPos_Bathroom_TopLeft, _camPos_Bathroom_BottomRight) 
+
+func setCameraToBedroom():
+	cameraManager.limitCameraToPositions(_camPos_Bedroom_TopLeft, _camPos_Bedroom_BottomRight) 
+
+func setCameraToStudio():
+	cameraManager.limitCameraToPositions(_camPos_Studio_TopLeft, _camPos_Studio_BottomRight) 
