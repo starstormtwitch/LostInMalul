89643a6450bee507b6f761867b0304d18a3d1eba
SLgeneration <slgeneration93@gmail.com>
2021-12-07 21:26:38 -0700

    moved all interact logic into contained location in textbox code
    all signals, and ui cancel event handling in the textbox code now

--- Untitled Game/src/Levels/House.gd
+++ Untitled Game/src/Levels/House.gd
@@ -58,9 +58,7 @@ func _on_Player_health_changed(_oldHealth, newHealth, maxHealth):
 
 # node to handle player input, and call the proper response
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed(EventsList.UI_CANCEL_EVENT):
-		_handleUICancelEvent()
-	elif event.is_action_pressed(EventsList.INTERACT_EVENT):
+	if event.is_action_pressed(EventsList.INTERACT_EVENT):
 		_handleInteractEvent()
 
 # Call when the "Interact" event is called. Will check which area player is in to
@@ -75,14 +73,6 @@ func _handleInteractEvent() -> void:
 	elif _garage_Teleport_Area.overlaps_body(_player):
 		setCameraToFoyerTeleport()
 
-# Call when the "UI_Cacnel" event is called. will close textboxes if open
-func _handleUICancelEvent() -> void:
-	if _textBox.isShowing:
-		_hideTextBox()
-
-func _hideTextBox(): 
-	_textBox.hideText()
-	get_tree().set_input_as_handled()
 
 func _on_VisibilityNotifier2D_screen_entered():
 	pass
