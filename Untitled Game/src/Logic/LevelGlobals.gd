8db931da3580be2208b3d812aae6cd4415031d92
Ed <emartine8742@gmail.com>
2022-02-22 00:25:18 +0100

    custom camera 2d changes
    create bounds around the camera limits that change with them when they update

--- Untitled Game/src/Logic/LevelGlobals.gd
+++ Untitled Game/src/Logic/LevelGlobals.gd
@@ -2,8 +2,13 @@ extends Node
 
 var _player: Actor
 
+var _layerDict: Dictionary = {}
+const _layerBitParamName: String = "bit"
+const _layerValueParamName: String = "value"
+
 func _ready():
 	_findPlayerEntity()
+	_createLayerDictionary()
 
 func SceneHasPlayerActor() -> bool:
 	return _player != null
@@ -23,3 +28,19 @@ func _findPlayerEntity() -> void:
 			_player = players[0]
 	else:
 		printerr("Parent does not exist.")
+
+func _createLayerDictionary() -> void:
+	for i in range(1, 32, 1):
+		var layer = ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(i))
+		if !layer.empty():
+			_layerDict[layer] = {
+				_layerBitParamName : i,
+				_layerValueParamName : pow(2, i-1)
+			}
+#	print(_layerDict)
+
+func GetLayerBit(name: String) -> int:
+	return _layerDict[name][_layerBitParamName]
+	
+func GetLayerValue(name: String) -> int:
+	return _layerDict[name][_layerValueParamName]
