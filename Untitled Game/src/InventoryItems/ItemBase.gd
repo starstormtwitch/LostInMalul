2b1966524fcb1be4398643572eafdff833cf2e2b
starstormtwitch <89739034+starstormtwitch@users.noreply.github.com>
2022-03-21 21:58:15 -0500

    tweak dropped item base so its more ready to use

--- Untitled Game/src/InventoryItems/ItemBase.gd
+++ Untitled Game/src/InventoryItems/ItemBase.gd
@@ -5,6 +5,7 @@ var _player : Actor
 func _init(player : Actor, item : Node2D):
 	_player = player;
 	$ItemContainer.add_child(item);
+	$InteractPromptArea.playerNodePath = _player.get_path()
 	pass;
 	
 # Called when the node enters the scene tree for the first time.
