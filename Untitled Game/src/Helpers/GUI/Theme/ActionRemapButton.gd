d60d30cf51cea5d6ebd08f03ff2698a16b1170c0
Ed <emartine8742@gmail.com>
2022-04-12 00:33:54 +0200

    minor improvements

--- Untitled Game/src/Helpers/GUI/Theme/ActionRemapButton.gd
+++ Untitled Game/src/Helpers/GUI/Theme/ActionRemapButton.gd
@@ -1,7 +1,7 @@
 extends Button
 
 export(String, "move_up", "move_down", "move_left", "move_right", "dodge", "special_attack", "side_swipe_attack", "attack_2") var action = "move_up"
-export(String, "mouseAndKeyboard", "controller") var input_type = "mouseAndKeyboard"
+export(String, "keyboard", "mouse", "controller") var input_type = "keyboard"
 
 var isEditing: bool = false
 
@@ -11,17 +11,15 @@ signal remap_keyInUse
 
 func _ready():
 	assert(InputMap.has_action(action))
-#	set_process_unhandled_key_input(false)
 	display_current_key()
 	add_to_group("RemapButton")
+	print(InputMap.get_action_list(action))
 
 func _toggled(button_pressed):
 	if isEditing:
 		return
-		
-	set_process_unhandled_input(button_pressed)
 	if button_pressed:
-		text = "input key..."
+		text = "..."
 		isEditing = true
 		release_focus()
 		emit_signal("remap_open")
@@ -29,52 +27,33 @@ func _toggled(button_pressed):
 		display_current_key()
 
 func _input(event: InputEvent) -> void:
-	if isEditing && event.is_pressed():
-		print(event)
-		if false: # event.is_action_pressed("ui_cancel"):
-			print("cancel")
+	if isEditing && event.is_pressed() && _isEventControlType(event):
+		if event.is_action_pressed("ui_cancel"):
 			isEditing = false
 			pressed = false
 		else:
 			remap_action_to(event)
-		
 
-#func _unhandled_key_input(event):
-#	print(event)
-#	# Note that you can use the _input callback instead, especially if
-#	# you want to work with gamepads.
-#	if isEditing:
-#		if event.is_action_pressed(EventsList.UI_CANCEL_EVENT):
-#			isEditing = false
-#		else:
-#			remap_action_to(event)
-#		pressed = false
+func _isEventControlType(event: InputEvent) -> bool:
+	return ((event is InputEventKey && self.input_type == "keyboard")
+		|| (event is InputEventMouseButton && self.input_type == "mouse")
+		|| (event is InputEventJoypadButton && self.input_type == "joypad"))
 
-func _checkEventIsInUse(event) -> bool:
-	var actions = InputMap.get_actions()
-	for action in actions:
-		if InputMap.action_has_event(action, event):
-			print("event in use")
-			return true
-	print("event not in use")
-	return false
-
-func _saveMapping(event):
-	InputFunctions.SaveCustomMapping(action, event)
+func _createMapping(event):
+	InputFunctions.CreateCustomMapping(action, event)
 
 func remap_action_to(event):
-	if !_checkEventIsInUse(event):
-		print("map event")
+	if InputFunctions.CheckCanUseCustomMapping(event):
 		InputMap.action_erase_events(action)
 		InputMap.action_add_event(action, event)
-		_saveMapping(event)
+		_createMapping(event)
 		display_current_key()
 	else:
-		print("key in use")
 		emit_signal("remap_keyInUse")
 	isEditing = false
 	pressed = false
 
 func display_current_key():
-	var inputMap: InputEvent = InputMap.get_action_list(action)[0]
+#	var inputMap: InputEvent = InputMap.get_action_list(action)[0]
+	var inputMap = InputFunctions.GetCustomMapping(action, input_type)
 	text = "%s" % InputFunctions.GetInputEventAsText(inputMap)
--- Untitled Game/src/Logic/InputFunctions.gd
+++ Untitled Game/src/Logic/InputFunctions.gd
@@ -7,24 +7,16 @@ const _inputTypeName_Mouse: String = "mouse"
 const _inputTypeName_Joypad: String = "joypad"
 
 
-static func GetInputEventAsText(event: InputEvent) -> String:
-	if event is InputEventMouseButton:
-		return MouseButtonIndexToText(event.button_index)
-	return event.as_text()
-	
-static func GetCustomMapping(action, inputType) -> InputEventKey:
+static func GetCustomMapping(action: String, inputType: String) -> InputEvent:
 	var data: Dictionary = GetCustomMappingData()
-	if !data.has(action) || !data[action].has(inputType):
-		return null
-	var key = data[action][inputType]
-	var inputEvent = InputEventKey.new()
-	inputEvent.set_scancode(OS.find_scancode_from_string(key))
-	return inputEvent
+	if data.has(action) && data[action].has(inputType):
+		return NewInputEventFrom(action, inputType, str(data[action][inputType]))
+	return null
 
-static func SaveCustomMapping(action, event):
+static func CreateCustomMapping(action: String, event: InputEvent):
 	var data: Dictionary = GetCustomMappingData()
-#	if !data.has(action): #for multi mapping
-	data[action] = {}
+	if !data.has(action):
+		data[action] = {}
 	if event is InputEventKey:
 		data[action][_inputTypeName_Keyboard] = OS.get_scancode_string(event.scancode)
 	elif event is InputEventMouseButton:
@@ -33,43 +25,74 @@ static func SaveCustomMapping(action, event):
 		push_error("Not implemented.")
 	SaveCustomMappingData(data)
 
+static func CheckCanUseCustomMapping(event: InputEvent) -> bool:
+	var data: Dictionary = GetCustomMappingData()
+	var eventString = GetInputEventValueAsString(event)
+	for actionName in data.keys():
+		for inputType in data[actionName]:
+			if str(data[actionName][inputType]) == eventString:
+				return false
+	return true
+
+static func GetInputEventValueAsString(event: InputEvent) -> String:
+	var result = ""
+	if event is InputEventKey:
+		result = OS.get_scancode_string(event.scancode)
+	elif event is InputEventMouseButton:
+		result = str(event.button_index)
+	elif event is InputEventJoypadButton || event is InputEventJoypadMotion:
+		push_error("Not implemented.")
+	return result
+
 static func LoadCustomMappings() -> void:
 	var data: Dictionary = GetCustomMappingData()
 	for actionName in data.keys():
-		InputMap.action_erase_events(actionName)
+		#InputMap.action_erase_events(actionName)
 		for inputTypeName in data[actionName]:
 			var inputEventValue: String = str(data[actionName][inputTypeName])
 			if !inputEventValue.empty():
-				if inputTypeName == _inputTypeName_Keyboard:
-					var inputEvent: InputEventKey = InputEventKey.new()
-					inputEvent.set_scancode(OS.find_scancode_from_string(inputEventValue))
+				var inputEvent = NewInputEventFrom(actionName, inputTypeName, inputEventValue)
+				if inputEvent != null:
 					InputMap.action_add_event(actionName, inputEvent)
-				elif inputTypeName == _inputTypeName_Mouse:
-					var inputEvent: InputEventMouseButton = InputEventMouseButton.new()
-					inputEvent.button_index = int(inputEventValue)
-					InputMap.action_add_event(actionName, inputEvent)
-				elif inputTypeName == _inputTypeName_Joypad:
-					push_error("Not implemented.")
+
+static func NewInputEventFrom(action: String, inputType: String, inputEventValue: String) -> InputEvent:
+	var inputEvent = null
+	if inputType == _inputTypeName_Keyboard:
+		inputEvent = InputEventKey.new()
+		inputEvent.set_scancode(OS.find_scancode_from_string(str(inputEventValue)))
+	elif inputType == _inputTypeName_Mouse:
+		inputEvent = InputEventMouseButton.new()
+		inputEvent.button_index = int(inputEventValue)
+	elif inputType == _inputTypeName_Joypad:
+		push_error("Not implemented.")
+	return inputEvent
+
+static func GetInputEventAsText(event: InputEvent) -> String:
+	if event == null:
+		return ""
+	elif event is InputEventMouseButton:
+		return MouseButtonIndexToText(event.button_index)
+	return event.as_text()
 
 static func MouseButtonIndexToText(index: int) -> String:
 	if index == BUTTON_LEFT || index == BUTTON_MASK_LEFT:
-		return "Left Mouse Button"
+		return "LMB" #"Left Mouse Button"
 	if index == BUTTON_RIGHT || index == BUTTON_MASK_RIGHT:
-		return "Right Mouse Button"
+		return "RMB" #"Right Mouse Button"
 	if index == BUTTON_MIDDLE || index == BUTTON_MASK_MIDDLE:
-		return "Middle Mouse Button"
+		return "MMB" #"Middle Mouse Button"
 	if index == BUTTON_XBUTTON1 || index == BUTTON_MASK_XBUTTON1:
-		return "Extra Mouse Button 1"
+		return "Thumb 1" #"Extra Mouse Button 1"
 	if index == BUTTON_XBUTTON2 || index == BUTTON_MASK_XBUTTON2:
-		return "Extra Mouse Button 2"
+		return "Thumb 2" #"Extra Mouse Button 2"
 	if index == BUTTON_WHEEL_UP:
-		return "Mouse Wheel Up"
+		return "Wheel Up" #"Mouse Wheel Up"
 	if index == BUTTON_WHEEL_DOWN:
-		return "Mouse Wheel Down"
+		return "Wheel Down" #"Mouse Wheel Down"
 	if index == BUTTON_WHEEL_LEFT:
-		return "Mouse Wheel Left"
+		return "Wheel Left" #"Mouse Wheel Left"
 	if index == BUTTON_WHEEL_RIGHT:
-		return "Mouse Wheel Right"
+		return "Wheel Right" #"Mouse Wheel Right"
 	return ""
 
 static func ResetInputMapping() -> void:
