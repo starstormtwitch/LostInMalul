f8d8411d449ed9648f64950296109e55163f6aba
SLgeneration <slgeneration93@gmail.com>
2022-05-10 17:27:30 -0600

    hurtbox now moves with sprite when knocbacked
    this is to stop palyer from constatnly attacking and knocking back
    enemy until they hit the roof. hurtbox position reset once landed

--- Untitled Game/src/Actors/Actor.gd
+++ Untitled Game/src/Actors/Actor.gd
@@ -4,6 +4,7 @@ class_name Actor
 # props of all objects in game
 const MAXVELOCITY = 500
 const _BOUNDARY_COLLISON_MASK_BIT = 0
+const HURTBOX_POSITION_OFFSET = 20
 
 var _velocity: = Vector2.ZERO
 var _acceleration = .2
@@ -17,6 +18,7 @@ var _groundPosition = self.position.y;
 
 #jump mechanic stuff
 var _lastYPostionOnGround = 0
+var _originalHurtBoxPosition: Vector2 = Vector2.ZERO
 
 var errorLogger = ErrorLogger.new()
 
@@ -24,18 +26,29 @@ signal health_changed
 signal died
 
 onready var kinematicSprite: KinematicBody2D = $KinematicSprite
+onready var hurtbox: Area2D = $HurtBox
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	errorLogger.assertNodeNotNull(kinematicSprite, "KinematicSprite", self)
+	errorLogger.assertNodeNotNull(hurtbox, "HurtBox", self)
+	_originalHurtBoxPosition = hurtbox.position
+
 
 func _physics_process(delta):
 	if(_inAir && _velocity.y > 0 && kinematicSprite.global_position.y >= _groundPosition):
-		_inAir = false;
-		self.global_position.y = _groundPosition;
-		kinematicSprite.global_position.y = _groundPosition;
+		_landOnGround()
 	if !_inAir:
 		_groundPosition = self.global_position.y
+	else:
+		hurtbox.global_position.y = kinematicSprite.global_position.y - HURTBOX_POSITION_OFFSET;
+
+
+func _landOnGround():
+	_inAir = false;
+	self.global_position.y = _groundPosition;
+	kinematicSprite.global_position.y = _groundPosition;
+	hurtbox.position = _originalHurtBoxPosition;
 
 
 #This is to move sprite only vertically, to make it look like its knockbacked.
