a3aa87f696bed458652d3e49dc1c4baf4c11a853
starstormtwitch <89739034+starstormtwitch@users.noreply.github.com>
2022-07-16 14:42:50 -0500

    fix dust trail

--- Untitled Game/src/Actors/MainChar/SmokeParticles.gd
+++ Untitled Game/src/Actors/MainChar/SmokeParticles.gd
@@ -5,5 +5,5 @@ class_name SmokeParticles
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#print("loading particle")
-	yield(get_tree().create_timer(2), "timeout")
+	yield(get_tree().create_timer(.5), "timeout")
 	queue_free()
